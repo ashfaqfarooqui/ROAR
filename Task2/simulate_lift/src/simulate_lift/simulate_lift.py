da83a8547c53031ffe63e6676763d2206c0d2119
Ashfaq <ashfaq.farooqui@gmail.com>
2015-09-09 10:34:24 +0200

    Added single robot stage simulation and made a service to simulate lift

--- Task2/simulate_lift/src/simulate_lift/simulate_lift.py
+++ Task2/simulate_lift/src/simulate_lift/simulate_lift.py
@@ -3,48 +3,43 @@ import roslib
 import rospy
 import time
 import sys
-from ROAR_msgs import *
-from sensor_msgs import JointState
+from roar_msg.srv import *
+from sensor_msgs.msg import JointState
 
 
 roslib.load_manifest('simulate_lift')
+rospy.init_node('simulate_lift')
 
-jointStatePub = rospy.Publisher('Joint_state',JointState,1)
 
-jointValue = 0
+
+jointValue = -0.78539816339
 def main():
-    rospy.init_node('simulate_lift')
+    jointStatePub = rospy.Publisher('joint_states', JointState, queue_size=5)
     rospy.Service('Simulate_Lift', SimulateLift, handleSimulateLift)
-
-    rospy.Subscriber('robot_state_publisher',callbackRSP)
-    rospy.spin()
-
-def callbackRSP(msg):
-    global jointValue
-    jointValue = msg.position(1)
+    rate = rospy.Rate(125)
+    while not rospy.is_shutdown():
+        msg = JointState()
+        msg.header.stamp = rospy.Time.now()
+        msg.name=['right_leg_right_arm','left_leg_left_arm']
+        msg.position=[jointValue,jointValue]
+        msg.velocity = []
+        msg.effort = []
+        jointStatePub.publish(msg)
+        rate.sleep()
 
 
 def handleSimulateLift(req):
-    msg = JointState()
-    jointVal = jointValue
-    if req.directionToMove == "up":
-        msg.name.append('right_leg_right_arm')
-        msg.name.append('left_leg_left_arm')
-        jointVal += 0.05
-        msg.position.append(jointVal)
-        msg.position.append(jointVal)
-        jointStatePub.publish(msg)
-        return True
-    else req.directionToMove == "down":
-        if req.directionToMove == "up":
-        msg.name.append('right_leg_right_arm')
-        msg.name.append('left_leg_left_arm')
-        jointVal -= 0.05
-        msg.position.append(jointVal)
-        msg.position.append(jointVal)
-        jointStatePub.publish(msg)
-        return True
-    return False
+    if req.directionToMove == "down":
+        global jointValue
+        jointValue += 0.1
+    elif req.directionToMove == "up":
+        global jointValue
+        jointValue -= 0.1
+    if(jointValue > 0.78539816339):
+        jointValue = 0.78539816339
+    elif(jointValue < -0.78539816339):
+        jointValue = -0.78539816339
+    return True
 
 if __name__ == '__main__':
     main()
