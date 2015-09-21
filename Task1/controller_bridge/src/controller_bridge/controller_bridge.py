a58df6908988c2ec93460222370f25c7d8a3b6fb
Ashfaq <ashfaq.farooqui@gmail.com>
2015-09-21 22:50:07 +0200

    merged all python files n nodes into single files

--- Task1/controller_bridge/src/controller_bridge/controller_bridge.py
+++ Task1/controller_bridge/src/controller_bridge/controller_bridge.py
@@ -1,4 +1,65 @@
+#!/usr/bin/env python
+import roslib
+import rospy
+import optparse
+
 import requests
+
+
+
+from roar_msg.msg import Event
+
+roslib.load_manifest('controller_bridge')
+activeOperations = []
+
+
+def callbackupEventMsg(uc_evt):
+    rospy.loginfo("UC evt recieved:" + uc_evt.Event)
+    ControllerBridge.getInstance().postTransition(uc_evt.Event)
+    print uc_evt.Event[3:]
+    activeOperations.remove(uc_evt.Event[3:])
+
+
+def main():
+
+    UID = lastUID = 0
+    # Parses command line arguments
+    parser = optparse.OptionParser(usage="usage: %prog hostname port")
+    (options, args) = parser.parse_args(rospy.myargv()[1:])
+    if len(args) < 1:
+        controller_hostname = 'localhost'
+        port = 8080
+    elif len(args) == 1:
+        controller_hostname = args[0]
+        port = 8080
+    elif len(args) == 2:
+        controller_hostname = args[0]
+        port = int(args[1])
+        if not (0 <= port <= 65535):
+                parser.error("You entered an invalid port number")
+    else:
+        parser.error("Wrong number of parameters")
+
+    rospy.init_node('ControllerBridge')
+    rate = rospy.Rate(2)
+    eventPublisher = rospy.Publisher('Events', Event, queue_size=5)
+    rospy.Subscriber('uc_Events', Event, callbackupEventMsg)
+    cb = ControllerBridge.getInstance(controller_hostname,port)
+    while not rospy.is_shutdown():
+        lastUID = UID
+        operations = cb.getExecutingOperations()
+        UID = cb.getCurrentID()
+        if UID != lastUID:
+            for operation in operations:
+                if operation not in activeOperations:
+                    eventMsg = Event()
+                    eventMsg.Event = operation
+                    eventMsg.UID = UID
+                    rospy.loginfo(eventMsg)
+                    eventPublisher.publish(eventMsg)
+                    rospy.loginfo("operation completed with UID:" + str(UID))
+                    activeOperations.append(operation)
+        rate.sleep()
 
 
 class ControllerBridge:
@@ -42,3 +103,6 @@ class ControllerBridge:
             print "instance"
             ControllerBridge.__bridgeInstance = ControllerBridge(host, port)
         return ControllerBridge.__bridgeInstance
+
+if __name__ == '__main__':
+    main()
--- Task1/roar_bringup/src/roar_bringup/bringup.py
+++ Task1/roar_bringup/src/roar_bringup/bringup.py
@@ -2,12 +2,192 @@
 import roslib;
 import rospy
 import json
-from roar_bringup.robot import *
 from visualization_msgs.msg import *
 from roar_msg.msg import *
 
+import time
+import threading 
+from geometry_msgs.msg import Point
+
 roslib.load_manifest('roar_bringup')
 
+# objects:
+class Robot:
+    def __init__(self,robotName,position):
+        self.initialPosition = position
+        self.robotName = robotName
+        self.pathPublisher = rospy.Publisher('path',Marker,queue_size=1)
+        self.marker = Marker()
+        self.marker.header.frame_id = "/neck"
+        self.marker.type = self.marker.CUBE
+        self.marker.action = self.marker.ADD
+        self.marker.scale.x = 0.2
+        self.marker.scale.y = 0.5
+        self.marker.scale.z = 0.2
+        self.marker.color.a = 1.0
+        self.marker.color.r = 1.0
+        self.marker.color.g = 1.0
+        self.marker.color.b = 0.5
+        self.marker.pose.orientation.w = 1.0
+        self.marker.pose.position.x = position.x
+        self.marker.pose.position.y = position.y
+        self.marker.pose.position.z = position.z
+        self.marker.lifetime = rospy.Duration(0.2)
+
+
+    def getName(self):
+        return self.robotName
+
+    def  updatePosition(self, newPosition):
+        self.marker.pose.position.x = newPosition.x
+        self.marker.pose.position.y = newPosition.y
+        self.marker.pose.position.z = newPosition.z
+
+    def attachBin(self, bin):
+        self.marker.color.a = 1.0
+        self.marker.color.r = 1.0
+        self.marker.color.g = 1.0
+        self.marker.color.b = 1.0
+        bin.setConnectedRobot(self)
+
+    def dettachBin(self,bin):
+        self.marker.color.a = 1.0
+        self.marker.color.r = 1.0
+        self.marker.color.g = 1.0
+        self.marker.color.b = 0.5
+        bin.disconnectRobot()
+
+    def getMarker(self):
+        return self.marker
+
+    def getInitialPosition(self):
+        return self.initialPosition
+
+    def moveTo(self,obj):
+        self.moveToDestination(obj.getInitialPosition())
+
+    def moveToDestination(self, destination):
+        self.lineList = Marker()
+        self.lineList.header.frame_id = "/neck"
+        self.lineList.type = self.marker.LINE_STRIP
+        self.lineList.action = self.marker.ADD
+        self.lineList.scale.x = 0.2
+        self.lineList.scale.y = 0.5
+        self.lineList.scale.z = 0.2
+        self.lineList.color.a = 1.0
+        self.lineList.color.r = 1.0
+        self.lineList.color.g = 0.5
+        self.lineList.color.b = 0.5
+        self.lineList.pose.orientation.w = 1.0
+        self.lineList.lifetime = rospy.Duration(5)
+        self.lineList.points.append(self.marker.pose.position)
+        self.lineList.points.append(destination)
+        self.pathPublisher.publish(self.lineList)
+        self.updatePosition(Point((self.marker.pose.position.x + destination.x)/2,(self.marker.pose.position.y + destination.y)/2,0))
+        rospy.sleep(5)
+        self.updatePosition(destination)
+
+class Truck():
+    def __init__(self,truckName,position):
+        self.initialPosition = position
+        self.truckName = truckName
+        self.marker = Marker()
+        self.marker.header.frame_id = "/neck"
+        self.marker.type = self.marker.CUBE
+        self.marker.action = self.marker.ADD
+        self.marker.scale.x = 1.2
+        self.marker.scale.y = 0.5
+        self.marker.scale.z = 0.1
+        self.marker.color.a = 1.0
+        self.marker.color.r = 0.7
+        self.marker.color.g = 0.3
+        self.marker.color.b = 0.5
+        self.marker.pose.orientation.w = 1.0
+        self.marker.pose.position.x = position.x
+        self.marker.pose.position.y = position.y
+        self.marker.pose.position.z = position.z
+        self.marker.lifetime = rospy.Duration(0.2)
+
+    def getName(self):
+        return self.truckName
+
+    def getInitialPosition(self):
+        return self.initialPosition
+
+    def emptyBin(bin):
+        bin.setEmptyStatus(True)
+
+    def getMarker(self):
+        return self.marker
+
+class Bin(object):
+    def __init__(self,binName,position):
+        self.isEmpty = False
+        self.initialPosition = position
+        self.connectedRobot=None
+        self.isAttached = False
+        self.binName = binName
+        self.marker = Marker()
+        self.marker.header.frame_id = "/neck"
+        self.marker.type = self.marker.CYLINDER
+        self.marker.action = self.marker.ADD
+        self.marker.scale.x = 0.2
+        self.marker.scale.y = 0.2
+        self.marker.scale.z = 0.5
+        self.marker.color.a = 1.0
+        self.marker.color.r = 1.0
+        self.marker.color.g = 0.5
+        self.marker.color.b = 0.5
+        self.marker.pose.orientation.w = 1.0
+        self.marker.pose.position.x = position.x
+        self.marker.pose.position.y = position.y
+        self.marker.pose.position.z = position.z
+        self.marker.lifetime = rospy.Duration(0.2)
+        self.__thread = threading.Thread(name=binName+"_thread", target=self.__keepAlive)
+        self.__update_lock = threading.Lock()
+        self.__thread.daemon = True
+        self.__thread.start()
+
+    def setEmptyStatus(self,status):
+        self.marker.color.r = 0.5
+        self.marker.color.g = 0.5
+        self.marker.color.b = 0.5
+        self.isEmpty = status
+
+    def getEmptyStatus(self):
+        return self.isEmpty
+
+    def getName(self):
+        return self.binName
+
+    def getInitialPosition(self):
+        return self.initialPosition
+
+    def __keepAlive(self):
+        while not rospy.is_shutdown():
+            if self.isAttached == True:
+                rospy.loginfo("Updating bin position")
+                self.marker.pose.position.x = self.connectedRobot.marker.pose.position.x
+                self.marker.pose.position.y = self.connectedRobot.marker.pose.position.y
+                self.marker.pose.position.z = self.connectedRobot.marker.pose.position.z
+            rospy.sleep(0.1)
+
+    def getPosition(self):
+        return self.marker.pose.position
+
+    def setConnectedRobot(self,robot):
+        self.isAttached = True
+        self.connectedRobot = robot
+
+    def disconnectRobot(self):
+        self.setConnectedRobot(None)
+        self.isAttached = False
+
+    def getMarker(self):
+        return self.marker
+
+
+# simulation
 class Simulation:
 
     def __init__(self):
@@ -73,7 +253,7 @@ class Simulation:
                         tasks = evt.Event.split("_")
                         methodToCall = getattr(self.getObject(tasks[0]),tasks[1])
                         methodToCall(self.getObject(tasks[2]))
-                    except e:
+                    except Exception, e:
                         rospy.loginfo(e)
                     finally:
                         self.sendUCEvent(evt)
